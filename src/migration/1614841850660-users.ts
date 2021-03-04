3dcd9db71b534959282677329405a03120816989
Jisang Park <qkrwltkd99@gmail.com>
2021-03-04 16:13:35 +0900

    Added src/ormconfig.ts

---
+++ src/migration/1614841850660-users.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class users1614841850660 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
