b74b94cf1f34b72d1c66ab148710b852dbdccd14
Jisang Park <qkrwltkd99@gmail.com>
2021-03-04 17:20:29 +0900

    Added user migration

--- src/migration/1614841850660-users.ts
+++ src/migration/1614841850660-users.ts
@@ -1,7 +1,39 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class users1614841850660 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'nickname',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'social',
+            type: 'varchar',
+          },
+          {
+            name: 'createdAt',
+            type: 'datetime',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
 }
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -9,6 +9,7 @@ const config: ConnectionOptions = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: 'yeongn',
+  timezone: '+09:00',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
 };
 
