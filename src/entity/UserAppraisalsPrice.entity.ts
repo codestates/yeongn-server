73642300c855593f6921c87260f494206b11aff4
Jisang Park <qkrwltkd99@gmail.com>
2021-03-05 17:19:58 +0900

    Added entity usersAppraisalsPrices

--- src/entity/UserAppraisalsPrice.entity.ts
+++ src/entity/UserAppraisalsPrice.entity.ts
@@ -1,6 +1,10 @@
-import {Entity} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'usersAppraisalsPrices' })
 export class UserAppraisalsPrice {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column()
+  price: number;
 }
