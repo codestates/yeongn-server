501833e0efbe2c9ed9a57d33ba616f7dc45756e9
Jisang Park <qkrwltkd99@gmail.com>
2021-03-05 17:09:12 +0900

    Added entity appraisalsComments

--- src/entity/AppraisalsComment.entity.ts
+++ src/entity/AppraisalsComment.entity.ts
@@ -1,6 +1,13 @@
-import {Entity} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'appraisalsComments' })
 export class AppraisalsComment {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column()
+  text: string;
+
+  @Column({ type: 'datetime', default: 'now()' })
+  createdAt: Date;
 }
