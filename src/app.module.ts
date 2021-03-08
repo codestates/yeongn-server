ec04bcd418bb7b21347c34f896d66d7b5b8af4ea
=chiemsee30 <jetignis@gmail.com>
2021-03-08 17:55:03 +0900

    mod and delete

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,24 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { JwtService } from './jwt/jwt.service';
 import { AppraisalController } from './appraisal/appraisal.controller';
+import { AppraisalModule } from './appraisal/appraisal.module';
+import { AppraisalService } from './appraisal/appraisal.service';
 import { ShopController } from './shop/shop.controller';
 import { ShopService } from './shop/shop.service';
 import { ShopModule } from './shop/shop.module';
 import { AppraisalService } from './appraisal/appraisal.service';
 import { AppraisalModule } from './appraisal/appraisal.module';
+import { AppraisalController } from './appraisal/appraisal.controller';
 import * as ormconfig from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), UserModule, ShopModule, AppraisalModule],
-  controllers: [AppController, AppraisalController, ShopController],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    UserModule,
+    ShopModule,
+    AppraisalModule,
+  ],
+  controllers: [AppController],
   providers: [AppService, JwtService, ShopService, AppraisalService],
 })
 export class AppModule {}
